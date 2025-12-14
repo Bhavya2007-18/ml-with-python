533b4c0ccd88fca617c97f0a4a1dece5a356f137
Bhavya2007-18 <bhavyaporwal007@gmail.com>
2025-12-15 00:52:06 +0530

    basic info

---
+++ 1.1_INTRO.py
@@ -0,0 +1,19 @@
+# AI -- ML -- DL
+
+# Ml algo are classified into 4 types: 
+# 1. Supervised Learning -- models are trained using labeled data , uses loss function to minimize error
+# 2. Unsupervised Learning -- have unlabelled data , not have outcome variable feeded , have to discover hidden pattern , structures byself
+# 3. Reinforcement Learning -- learns by interacting with environment , feedback is given in terms of rewards or penalties , works on the principle of markov decision process
+# 4. Evolutionary Algorithms -- searches for optimal solutions by evolving a set of solutions over genrations(like darwin's evolution)
+
+# STEPS TO MAKE A ML MODEL SUCESSFULLY:
+# 1. feature extractions -- selecting relevant features from raw data
+# 2. feature engineering -- transforming raw data into features that better represent the underlying problem to the predictive models
+# 3. model selection -- choosing the appropriate algorithm for the task
+# 4. training -- feeding data to the model to learn patterns
+# 5. evaluation -- assessing the model's performance using metrics
+# 6. hyperparameter tuning -- optimizing model parameters to improve performance
+# 7. deployment -- integrating the model into a production environment for real-world use
+
+
+
